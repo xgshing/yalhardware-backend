bbe636176e2e48cb986148c3b5700fe096bffe80
xgshing <xgshing@qq.com>
2026-01-02 13:34:23 +0800

    Add urls.py

--- config/urls.py
+++ config/urls.py
@@ -5,23 +5,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# Django REST framework（DRF）后端里，没有像 Postman 或 Swagger 那样
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-
-# Swagger schema 配置
-schema_view = get_schema_view(
-   openapi.Info(
-      title="YAL Hardware API",
-      default_version='v1',
-      description="Frontend 可用的 API 文档",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,12 +13,7 @@ urlpatterns = [
     path('api/admin/', include('apps.content.urls.admin')),
     path('api/', include('apps.system.urls.company')),
     
-
-    # Swagger / Redoc 文档
-    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
-
 
 # 只有 DEBUG=True 时才允许直接访问 media
 if settings.DEBUG:
