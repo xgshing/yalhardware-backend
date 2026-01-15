e6492706c6e6d8da37ed96eb79dbd2513abc3eaf
xgshing <xgshing@qq.com>
2026-01-15 17:10:53 +0800

    设置生产环境禁用连接复用

--- config/settings.py
+++ config/settings.py
@@ -131,7 +131,7 @@ if DATABASE_URL:
     DATABASES = {
         "default": dj_database_url.config(
             default=DATABASE_URL,
-            conn_max_age=600,
+            conn_max_age=0,
             ssl_require=True,
         )
     }
